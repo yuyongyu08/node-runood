05bc8f1d052aeb37354361f57b79d1a26e0a7775
yuyongyu <yuyongyu@tiantonglaw.com>
2019-10-21 16:30:59 +0800

    modify

--- 2-event/1-eventEmitter.js
+++ 2-event/1-eventEmitter.js
@@ -2,20 +2,31 @@ const EventEmitter = require('events');
 class MyEmitter extends EventEmitter {}
 const myEmitter = new MyEmitter();
 
-function connectHandler() {
+myEmitter.on('connect', () => {
     console.log('连接建立');
 
-    myEmitter.emit('data_received')
-}
+    myEmitter.emit('dataReceived');
 
-function dataReceivedHandler(){
+    myEmitter.emit('closeConnect');
+    myEmitter.emit('closeConnect');
+
+});
+
+myEmitter.on('dataReceived', () => {
     console.log('接收到数据')
-}
+});
 
-myEmitter.on('connect', connectHandler);
-
-myEmitter.on('data_received', dataReceivedHandler);
+myEmitter.once('closeConnect', () => {
+    console.log('关闭连接')
+});
 
 myEmitter.emit('connect');
 
 console.log('程序执行完毕')
+
+
+/** 
+ * 为什么“程序执行完毕”最后打印，而不是在第一行？
+*/
+
+
