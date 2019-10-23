b4533bba7a1a8d7ffddac9572862b053c9d2944e
yuyongyu <yuyongyu@tiantonglaw.com>
2019-10-23 10:05:21 +0800

    add global

---
+++ 6-global/1-global.js
@@ -0,0 +1,21 @@
+Object.keys(global).forEach((key, index) => {
+    console.log(`${index}.${key}:`)
+});
+
+
+// 0.console:
+// 1.DTRACE_NET_SERVER_CONNECTION:
+// 2.DTRACE_NET_STREAM_END:
+// 3.DTRACE_HTTP_SERVER_REQUEST:
+// 4.DTRACE_HTTP_SERVER_RESPONSE:
+// 5.DTRACE_HTTP_CLIENT_REQUEST:
+// 6.DTRACE_HTTP_CLIENT_RESPONSE:
+// 7.global:
+// 8.process:
+// 9.Buffer:
+// 10.clearImmediate:
+// 11.clearInterval:
+// 12.clearTimeout:
+// 13.setImmediate:
+// 14.setInterval:
+// 15.setTimeout:
